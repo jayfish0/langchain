e958a846663180f724af2953abbd4261832d2816
jayfish0 <jason@tinyfish.io>
2025-01-23 16:49:02 -0800

    add util

---
+++ libs/community/langchain_community/tools/agentql/utils.py
@@ -0,0 +1,46 @@
+import agentql
+from typing import TYPE_CHECKING, Optional, List
+from playwright.sync_api import Browser as SyncBrowser
+from playwright.sync_api import Page as SyncPage
+
+if TYPE_CHECKING:
+    from playwright.async_api import Browser as AsyncBrowser
+    from playwright.async_api import Page as AsyncPage
+    from playwright.sync_api import Browser as SyncBrowser
+    from playwright.sync_api import Page as SyncPage
+
+def get_current_page(browser: SyncBrowser) -> SyncPage:
+    """
+    Get the current page of the browser.
+    Args:
+        browser: The browser to get the current page from.
+
+    Returns:
+        SyncPage: The current page.
+    """
+    if not browser.contexts:
+        context = browser.new_context()
+        return agentql.wrap(context.new_page())
+    context = browser.contexts[0]  # Assuming you're using the default browser context
+    if not context.pages:
+        return agentql.wrap(context.new_page())
+    # Assuming the last page in the list is the active one
+    return agentql.wrap(context.pages[-1])
+
+# def create_sync_playwright_browser(
+#     headless: bool = True, args: Optional[List[str]] = None
+# ) -> SyncBrowser:
+#     """
+#     Create a playwright browser.
+
+#     Args:
+#         headless: Whether to run the browser in headless mode. Defaults to True.
+#         args: arguments to pass to browser.chromium.launch
+
+#     Returns:
+#         SyncBrowser: The playwright browser.
+#     """
+#     from playwright.sync_api import sync_playwright
+
+#     browser = sync_playwright().start()
+#     return browser.chromium.launch(headless=headless, args=args)
